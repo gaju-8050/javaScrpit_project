72e765e5f942e26cdb1dfeb6cae1eff8410eb965
gaju-8050 <108262877+gaju-8050@users.noreply.github.com>
2024-11-06 09:59:30 +0000

    2nd classes commited

---
+++ 01_basics/01_varibales.js
@@ -0,0 +1,20 @@
+var accountId = 12234556;
+let accountName = "CHANDUchaiwala";
+const accountEmail = "chanduChaiwala18@gmail.com";
+accountPassword = "wdadc";
+accountCity = "Bengaluru";
+
+accountName = "RakhiSaSTEwALI";
+//accountEmail = "gmail.com"
+var accountid = 21;
+
+console.table([
+  accountCity,
+  accountEmail,
+  accountId,
+  accountName,
+  accountPassword,
+  accountid,
+]);
+
+/* dont use var because of issue of block scope and functional scope */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // newer  version of javaScrpit
+
+// number = 2 to power of 53
+// BigInt
+// string = ""
+// boolean = true/false
+// null = standlone value
+// undefined = 
+// symbol = unique 
+ let age = "things"
+
+console.info(typeof object)
