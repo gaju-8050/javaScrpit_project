e7acd18a49230b5272c3589dbdd8d54840c44653
gaju-8050 <108262877+gaju-8050@users.noreply.github.com>
2024-11-11 05:12:16 +0000

    DataTypes Converstion

---
+++ 01_basics/03_Conversion_Operation.js
@@ -0,0 +1,19 @@
+let score = "what a shot";
+
+console.log(typeof score)
+console.log(typeof (score))
+
+
+let valueInNumber = String(score)
+console.log(typeof score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+
+let duo = 33;
+
+let StringNumber = String(duo)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
