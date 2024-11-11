b5c0feeb0566347e8cfd60f829d0c00be84b6c7d
gaju-8050 <108262877+gaju-8050@users.noreply.github.com>
2024-11-11 09:37:51 +0000

    Converstion Changes

--- 01_basics/03_Conversion_Operation.js
+++ 01_basics/03_Conversion_Operation.js
@@ -17,3 +17,16 @@ let StringNumber = String(duo)
 console.log(StringNumber);
 console.log(typeof StringNumber);
 
+
+
+/* ---------------------------------------OPERATIONS ---------------------------------- */
+console.log(+true)
+console.log(true)
+console.log(-true)
+ 
+console.log(+false)
+console.log(false)
+console.log(-false)
+ 
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
