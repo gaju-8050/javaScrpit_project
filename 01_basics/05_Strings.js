e92b44483d19456758ad3cf0dfa9e20d5280abf9
gaju-8050 <108262877+gaju-8050@users.noreply.github.com>
2024-11-12 10:13:18 +0000

    Strings Changes

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,20 @@
+ const kya_bol_raha_hai = " ba ba bahinchod"
+ 
+ console.log(`A ji Gali de raha hai ${kya_bol_raha_hai} bol raha hai  `);
+
+
+ const gadi_nikal = new String('chabi kaha hai')
+console.log(gadi_nikal);
+
+
+console.log(gadi_nikal[12]);
+
+
+const newString = gadi_nikal.substring(0,7)
+console.log(newString);
+
+const newString2 = gadi_nikal.slice(-1,1)
+console.log(newString2);
+
+const babyDoll = new String(`jo-dekh%raha%hai%woh-chutiya-hai%`)
+console.log(babyDoll.split(`%`));
